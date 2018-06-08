1fc1a0264f8b02677ab28db4d988c19287ab809c
Travis <travisjw@gmail.com>
2018-06-08 02:14:04 -0700

    Show/Hide blog button now fully functional.

---
+++ Travis-Amanda/ReadMe.md
@@ -0,0 +1,17 @@
+# Kilvolt Blog, Thursday
+
+**Author**: Travis Williams
+**Version**: 2.0.2 (increment the patch/fix version number up if you make more commits past your first submission)
+
+## Overview
+A blog project, mostly intended as a cool and convenient way of learning jQuery.  Also, a humbling reminder of the fact that I'm often immensely stupid.  Problem domain is to dynamically render blog articles to the page from a blog object, and dynamically show/hide content based on if read-more/read-less is clicked, as well as Home/About in the nav bar.
+
+## Getting Started
+If you found this, it means you know where it lives on github.  Go ahead and fork.
+
+## Architecture
+jQuery.  
+Also, tears and caffeine.
+
+## Change Log
+Probably should have done this.  Didn't.
--- Travis-Amanda/scripts/articleView.js
+++ Travis-Amanda/scripts/articleView.js
@@ -79,18 +79,23 @@ articleView.handleMainNav = function() {
   // DONE: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-  $('.tab[data-content="articles"]').on('click', function (event) {
-    if (event.target.children[0].className)
-    console.log(event);
-    debugger;
+  $('.tab').on('click', function (event) {
+    
+    if ( $(event)[0].target.className === 'icon-home'){
+      $('#about').hide();
+      $('article').show();
+      $('.template').hide();
+      $('#category-filter').val('');
+      $('#author-filter').val('');
+    }
+    else if ( $(event)[0].target.className === 'icon-address-book'){
+      $('article').hide();
+      $('#about').show();
+      $('#category-filter').val('');
+      $('#author-filter').val('');
+    }
   })
   
-
-  // let $newLocation=$(this).data('tab');
-  $('article').hide();
-  $('#about').hide();
-  // $(`#${$newLocation}`).show();
-
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('nav .tab:first').click();
 };
@@ -102,11 +107,16 @@ articleView.setTeasers = function() {
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
   
-
-
   $('#articles').on('click', '.read-on', function(event) {
     event.preventDefault();
-    $(this).parent().find("*").show();
+    if (this.innerHTML=== 'Read on →'){
+      $(this).parent().find("*").show();
+      this.innerHTML= 'read-less ↑';
+    }
+    else {
+      $(this).parent().find("*:nth-of-type(n+2)").hide();
+      this.innerHTML='Read on →';
+    }
   });
 };
 
